bda52f3b91eb210411b5034bf6b4496654ec322e
art0254 <nana.ort@gmail.com>
2022-06-03 22:28:41 +0700

    Add Preview Image

--- components/layout/page-container.jsx
+++ components/layout/page-container.jsx
@@ -5,6 +5,10 @@ const PageContainer = ({ title = "", description = "", children }) => {
       <Head>
         <title>{title ? title + " | " : ""} DHAMA RADIO - ฟังธรรมออนไลน์</title>
         <meta name='description' content={description ? description : title} />
+        <meta name='keywords' content={description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:image' content='/images/preview.png' />
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
       </Head>
       {children}
     </>
